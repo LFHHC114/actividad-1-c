9778f54e4f2d044f4c1d192a4e4dc6ef7aafb12f
actividad-c- <luuisahurtado2005@gmail.com>
2026-02-16 20:02:39 -0500

    actividad de c#

---
+++ reto_tipos_de_datos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Declaración de variables
+        int age = 25;
+        decimal height = 1.75m;
+        bool is_student = true;
+        string name = "Ana";
+        char grade = 'A';
+
+        // Mostrar variables en consola
+        Console.WriteLine("La edad es: " + age);
+        Console.WriteLine("La estatura es: " + height);
+        Console.WriteLine("¿Es estudiante?: " + is_student);
+        Console.WriteLine("El nombre es: " + name);
+        Console.WriteLine("La calificación es: " + grade);
+    }
+}
+
---
+++ reto_tipos_de_operadores/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int width = 7;
+        int height = 5;
+
+        int area = width * height;
+        int perimeter = 2 * width + 2 * height;
+
+        width += 3;
+        height -= 2;
+
+        area = width * height;
+        perimeter = 2 * width + 2 * height;
+
+        Console.WriteLine("El ancho final es: " + width);
+        Console.WriteLine("El alto final es: " + height);
+        Console.WriteLine("El área final es: " + area);
+        Console.WriteLine("El perímetro final es: " + perimeter);
+    }
+}
